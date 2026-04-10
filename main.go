7c6db8f9f133980a09a5aab0369ed20bb943a84e
selin-karaman <selinkaraman02@gmail.com>
2026-04-11 00:02:26 +0300

    add K8s deployment manifests

--- main.go
+++ main.go
@@ -10,60 +10,68 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	fmt.Println("[KubeCheck] Validating cluster connection...")
+	fmt.Println("[KubeCheck] Starting Cluster Health Monitor...")
 
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "path to kubeconfig")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "path to kubeconfig")
-	}
-	flag.Parse()
+	var config *rest.Config
+	var err error
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err = rest.InClusterConfig()
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		fmt.Println("Running outside cluster, looking for kubeconfig...")
+
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "path to kubeconfig")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "path to kubeconfig")
+		}
+		flag.Parse()
+
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			fmt.Printf("Fatal Error: Could not load any config: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("Running inside Kubernetes cluster! Using ServiceAccount.")
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		fmt.Printf("Error building clientset: %v\n", err)
+		os.Exit(1)
 	}
 
 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		fmt.Printf("Error listing pods: %v\n", err)
+		fmt.Println("Check if your RBAC (Role/ServiceAccount) permissions are correct.")
+		os.Exit(1)
 	}
 
 	fmt.Printf("\n CLUSTER HEALTH REPORT\n")
-	
-	// Go'nun standart tabwriter yapısını kuralım
 	w := tabwriter.NewWriter(os.Stdout, 10, 1, 3, ' ', 0)
 	fmt.Fprintln(w, "NAMESPACE\tNAME\tSTATUS\tHEALTH")
 	fmt.Fprintln(w, "---------\t----\t------\t------")
 
 	unhealthyCount := 0
-
 	for _, pod := range pods.Items {
 		status := string(pod.Status.Phase)
 		health := "Healthy"
-		
+
 		if status != "Running" && status != "Succeeded" {
 			health = "Unhealthy"
 			unhealthyCount++
 		}
-
 		fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", pod.Namespace, pod.Name, status, health)
 	}
-	w.Flush() 
+	w.Flush()
 
 	if unhealthyCount == 0 {
 		fmt.Println("\n Cluster Status: PERFECT")
